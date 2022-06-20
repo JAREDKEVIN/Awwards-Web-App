782f5ec2f79c86c822463925168d03055819aa78
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 17:11:39 +0300

    ft: modified reister update  function

--- awards/views.py
+++ awards/views.py
@@ -67,4 +67,57 @@ def update(request):
             p_form.save()
             messages.success(request, f'Successfully updated your account!')
             return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'update.html', context)
+
+@login_required
+def upload_project(request):
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == "POST":
+        form = ProjectUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit = False)
+            project.save()
+            messages.success(request, f'Successfully uploaded your Project!')
+            return redirect('home')
+    else:
+        form = ProjectUploadForm()
+    return render(request, 'upload_project.html', {"form": form, "users": users})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Successfully created account created for {username}! Please log in to continue')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form':form})
+
+@login_required
+def rate(request, project_id):
+    project = Project.objects.get(id=project_id)
+    user = request.user
+    if request.method == 'POST':
+        form = RateForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = user
+            rate.project = project
+            rate.save()
+        return render(request, 'project.html', locals())
+    else:
+        form = RateForm()
+    return render(request, 'rate.html', locals())
+
+
+
 
