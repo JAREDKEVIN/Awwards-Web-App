b0ed15231d16ed5bd3f31706038258feddaa312f
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 16:51:32 +0300

    ft: modify url patterns/paths

--- awards/urls.py
+++ awards/urls.py
@@ -3,7 +3,19 @@ from . import views
 from django.contrib.auth import views as auth_views
 from .views import ListProfileView, ListProjectView
 
-urlpatterns = [
-path('',views.home,name = 'home'),
-
+urlpatterns=[
+    # re_path('^$',views.welcome,name = 'welcome'),
+    path('',views.home,name = 'home'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('search/', views.search_results, name='search_results'),
+    path('profile/', views.profile, name='profile'),
+    path('update/', views.update, name='update'),
+    path('upload_project/', views.upload_project, name='upload_project'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('rate/<project_id>/',views.rate,name ='rate'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('api/profile/', ListProfileView.as_view(), name="profile-all"),
+    path('api/project/', ListProjectView.as_view(), name="project-all"),
 ]
