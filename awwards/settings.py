3f46e10bdcceabd64d4374688db9b20f3939324c
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-21 01:21:41 +0300

    configure database allowed hosts

--- awwards/settings.py
+++ awwards/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'x%_jcw%#a$qsv8)eyh+f_^#!xzx($wm0xrlk9!^(h=pjz*q3g='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ['awwardskipkemoi.herokuapp.com','localhost',]
+ALLOWED_HOSTS = ['awwardskipkemoi.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': 'awwards',
         'USER': 'postgres',
         'PASSWORD':'SEMBERUA',
-        'HOST':'',
+        'HOST':'localhost',
         'PORT': '',
     }
 }
