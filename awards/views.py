df5a2b9d7032f5aea0c3feac59254c7ca4a67d76
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 16:23:39 +0300

    modify url path

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    projects = Project.objects.all()
+    users = User.objects.all()
+    return render(request, 'home.html', {"projects":projects, "users": users})
