3ce6a7d73bebb8b7b211d63cf3ec83e20ea2c70e
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 17:17:32 +0300

    ft: modified models structure

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.dispatch import 
 
 # Create your models here.
