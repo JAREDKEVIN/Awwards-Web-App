72a4fd1094a9f09590275f23e4d1ed165b0508ac
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 18:22:51 +0300

    modify models file

--- awards/models.py
+++ awards/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
-# Create your models here.
 
+# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     bio = models.TextField(null=True)
@@ -22,19 +21,18 @@ class Project(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     prof_ref = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects', null=True)
 
-class Meta:
+    class Meta:
     
         ordering = ['pub_date']
 
-def __str__(self):
+    def __str__(self):
         return self.title
 
-def no_of_ratings(self):
+    def no_of_ratings(self):
         ratings = Rating.objects.filter(project=self)
         return len(ratings)
 
-
-def average_ratings(self):
+    def average_ratings(self):
         sum = 0
         ratings = Rating.objects.filter(project=self)
         for rating in ratings:
@@ -44,8 +42,11 @@ def average_ratings(self):
         else:
             return 0
 
-@classmethod
-def search_project(cls, search_term):
+
+
+
+    @classmethod
+    def search_project(cls, search_term):
         project = cls.objects.filter(title__icontains=search_term)
         return project
 
@@ -62,6 +63,7 @@ class Rating(models.Model):
     (2,'2-Dreadful'),
     (1,'1-Troll'),
 ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     review = models.TextField(null=True)
@@ -70,5 +72,5 @@ class Rating(models.Model):
     rate_content = models.PositiveSmallIntegerField(choices = RATE_CHOICES)
 
 
-def __str__(self):
+    def __str__(self):
         return self.user.username
