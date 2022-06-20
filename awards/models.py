c24bcb5f871e119dc4bc7cf55682eeb321878c3c
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-20 17:22:10 +0300

    create project function to display project details

--- awards/models.py
+++ awards/models.py
@@ -13,3 +13,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Project(models.Model):
+    title = models.CharField(max_length = 60)
+    project_image = CloudinaryField('project_image', null=True)
+    description = models.TextField()
+    link = models.CharField(max_length = 200, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    prof_ref = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects', null=True)
