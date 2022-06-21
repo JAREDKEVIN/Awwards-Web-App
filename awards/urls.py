384cbb6c9e9acb69fcf6728ab3dd5d3ae212569d
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2022-06-21 09:58:14 +0300

    create super user

--- awards/urls.py
+++ awards/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import re_path,path
 from . import views
 from django.contrib.auth import views as auth_views
@@ -18,4 +19,5 @@ urlpatterns=[
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('api/profile/', ListProfileView.as_view(), name="profile-all"),
     path('api/project/', ListProjectView.as_view(), name="project-all"),
+
 ]
